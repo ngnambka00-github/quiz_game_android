1716d6bb9e5e739c588ab234a4ef1fd205aba64a
thi <nguyenvietthi.ng@gmail.com>
2022-12-25 21:46:26 +0700

    new config.py

--- Server/config.py
+++ Server/config.py
@@ -3,7 +3,12 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = "dgsMUE35@123"
+app.config["MYSQL_DATABASE_PASSWORD"] = "admin"
 app.config["MYSQL_DATABASE_DB"] = "quiz_game"
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 mysql.init_app(app)
+
+UPLOAD_FOLDER = 'Server/static/avatar_image/'
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
